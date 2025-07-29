487155beb684bf54490243fb7747565fe10a0fb6
Jl Swamy <jlswamy8@gmail.com>
2025-07-29 22:37:40 +0530

    first problem

--- src/main/java/com/leetcode/dsa/easy/BuyAndSellStock.java
+++ src/main/java/com/leetcode/dsa/easy/BuyAndSellStock.java
@@ -2,6 +2,11 @@ package com.leetcode.dsa.easy;
 
 public class BuyAndSellStock {
     public static void main(String[] args) {
+        /*
+        You are given an array prices where prices[i] is the price of a given stock on the ith day.
+        You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+        Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+         */
         System.out.println(maxProfit(new int[]{7,6,4,3,1}));
     }
 
